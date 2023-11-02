86c61582273df1b1d9c199f97c9cc7cd854a878f
pd664 <63898008+pd664@users.noreply.github.com>
2023-11-02 16:49:18 +0530

    Update socket.js

--- lullu/src/socket.js
+++ lullu/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const URL = 'http://127.0.0.1:3000';
+const URL = 'https://lullumalls-c1fcb9537153.herokuapp.com/';
 
 export const socket = io(URL, {
     autoConnect: false
